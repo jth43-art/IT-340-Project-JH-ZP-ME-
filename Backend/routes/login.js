ce5e3814f638b2eda6754caa1d86a04f6720c82b
jth43-art <jth43@njit.edu>
2026-04-19 18:18:30 -0400

    Create login.js
    /login Express route

---
+++ Backend/routes/login.js
@@ -0,0 +1,6 @@
+//Used Copilot to construct basis for code
+const express = require("express");
+const router = express.Router();
+const { loginUser } = require("../controllers/loginController");
+router.post("/", loginUser);
+module.exports = router;

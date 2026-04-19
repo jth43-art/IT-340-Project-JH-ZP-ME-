0f0b8123dd6432d4c69ee9ed6aa8844215c5ea4a
jth43-art <jth43@njit.edu>
2026-04-19 18:29:11 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const loginRoute = require("./routes/login");
 
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use("/login", loginRoute);
 
 // ROUTES
 app.get('/now-playing', (req, res) => {

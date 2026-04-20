4039d9c3c7b4c2724773df6c322e3507d425d536
jth43-art <jth43@njit.edu>
2026-04-20 19:40:37 -0400

    Update loginController.js

--- Backend/controllers/loginController.js
+++ Backend/controllers/loginController.js
@@ -4,22 +4,28 @@ const bcrypt = require("bcrypt");
 exports.loginUser = async (req, res) => {
     try {
         const { identifier, password } = req.body;
-        // identifier = username OR email
+        if (!identifier || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        // Find user by username OR email
         const user = await User.findOne({
             $or: [{ username: identifier }, { email: identifier }]
         });
         if (!user) {
             return res.status(400).json({ message: "User not found" });
         }
+        // Compare password
         const validPassword = await bcrypt.compare(password, user.password);
         if (!validPassword) {
             return res.status(401).json({ message: "Invalid password" });
         }
         return res.status(200).json({
             message: "Login successful",
-            user: {username: user.username, email: user.email}
+            user: {
+                username: user.username,
+                email: user.email
+            }
         });
-
     } catch (err) {
         console.error("Login error:", err);
         res.status(500).json({ message: "Server error" });

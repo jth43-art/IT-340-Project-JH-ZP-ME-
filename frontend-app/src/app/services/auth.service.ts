fa3002a33fb09fe1d1f1f8cd88dff50cde90eea2
Zeel Patel <Zeel153>
2026-04-24 09:43:22 -0400

    adding frontend angular folder

---
+++ frontend-app/src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  // Use Jackson's Backend Tailscale IP here
+  private apiUrl = 'http://100.xxx.xxx.xxx:3000/auth'; 
+
+  constructor(private http: HttpClient) {}
+
+  register(userData: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, userData);
+  }
+
+  login(credentials: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, credentials);
+  }
+}

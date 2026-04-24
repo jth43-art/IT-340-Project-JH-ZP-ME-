8d0809df4bb5c0cbb3b8a12a3ff44983d1444f40
jth43-art <jth43@njit.edu>
2026-04-24 10:35:51 -0400

    Update register.js

--- Backend/routes/register.js
+++ Backend/routes/register.js
@@ -2,21 +2,7 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const {registerUser} = require("../controllers/registerController");
 const router = express.Router();
-
-router.post("/", async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const newUser = new User({
-      fullName: req.body.fullName,
-      email: req.body.email,
-      username: req.body.username,
-      password: hashedPassword
-    });
-    await newUser.save();
-    res.json({ message: "User registered successfully" });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-});
+router.post("/", registerUser);
 module.exports = router;

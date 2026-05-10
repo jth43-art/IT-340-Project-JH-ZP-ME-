4ba362eed37054b4828366b4a0acf1b78af90115
jth43-art <jth43@njit.edu>
2026-05-10 17:43:05 -0400

    Create mockAuth.js

---
+++ Backend/middleware/mockAuth.js
@@ -0,0 +1,9 @@
+module.exports = function mockAuth(req, res, next) {
+  // Pretend user is logged in
+  req.user = {
+    _id: "663a1f2c1234567890abcd12",
+    role: "user",
+    email: "test@example.com"
+  };
+  next();
+};

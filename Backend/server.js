52fc8775fc1e820e04316af76dd6930952ac9528
jth43-art <jth43@njit.edu>
2026-05-11 01:18:50 -0400

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,4 @@
 //Used Copilot to construct basis for code
-// server.js
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -9,7 +7,7 @@ const loginRoute = require('./routes/login');
 const registerRoute = require('./routes/register');
 const homepageRoute = require('./routes/homepage');
 const playlistRoute = require('./routes/playlists');
-const searchRoutes = require('./routes/search');
+const searchRoute = require('./routes/search');
 const mockAuth = require('./middleware/mockAuth');
 
 const app = express();

2e9d1c80db145ad41c70faf4d5e96fb8d809e134
jth43-art <jth43@njit.edu>
2026-05-12 11:00:18 -0400

    Update mockAuth.js
    Can switch roles now

--- Backend/middleware/mockAuth.js
+++ Backend/middleware/mockAuth.js
@@ -2,7 +2,7 @@ module.exports = function mockAuth(req, res, next) {
   // Pretend user is logged in
   req.user = {
     _id: "663a1f2c1234567890abcd12",
-    role: "user",
+    role: req.headers['x-role'] || 'user',
     email: "test@example.com"
   };
   next();

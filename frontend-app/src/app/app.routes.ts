112d2583c76718be82ac59af928a9a48f90bdf6d
matthewescobarr <mde27@njit.edu>
2026-05-12 17:38:18 -0400

    Add register route to app.routes.ts

--- frontend-app/src/app/app.routes.ts
+++ frontend-app/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+
 import LoginComponent from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 import { HomepageTvComponent } from './pages/homepage-tv/homepage-tv.component';
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
 import { SearchComponent } from './pages/search/search.component';
@@ -7,10 +9,18 @@ import { PlaylistComponent } from './pages/playlist/playlist.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+
+  { path: 'register', component: RegisterComponent },
+
   { path: 'homepage-tv', component: HomepageTvComponent },
+
   { path: 'admin-dashboard', component: AdminDashboardComponent },
+
   { path: 'search', component: SearchComponent },
+
   { path: 'playlists', component: PlaylistComponent },
+
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+
   { path: '**', redirectTo: '/login' }
 ];

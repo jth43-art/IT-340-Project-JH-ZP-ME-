ea6b8b4c3e12de1d647a9c5d90868cbf33ef0178
jth43-art <jth43@njit.edu>
2026-07-29 16:16:24 -0400

    Update mfaController.js
    Added 6-digit verification code & MFA Secret generation

--- Backend/controllers/mfaController.js
+++ Backend/controllers/mfaController.js
@@ -1,1 +1,23 @@
+const speakeasy = require("speakeasy");
+const qrcode = require("qrcode");
+exports.enableMFA = async (req, res) => {
+  const secret = speakeasy.generateSecret({ name: "TuneVault" });
+  const qr = await qrcode.toDataURL(secret.otpauth_url);
 
+  // Save secret to user in DB
+  user.mfaSecret = secret.base32;
+  await user.save();
+  res.json({ qr, secret: secret.base32 });
+};
+
+exports.verifyMFA = (req, res) => {
+  const { token } = req.body;
+  const verified = speakeasy.totp.verify({
+    secret: req.user.mfaSecret,
+    encoding: "base32",
+    token
+  });
+
+  if (!verified) return res.status(400).json({ message: "Invalid code" });
+  res.json({ message: "MFA enabled" });
+};

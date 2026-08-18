456fd10c4249b3712a82bf5457a10aac131573d1
Zeel <pzeel150302@gmail.com>
2026-08-18 11:39:02 -0400

    Implement UploadService for song uploads

---
+++ frontend-app/src/app/services/upload.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({ providedIn: 'root' })
+export class UploadService {
+  private baseUrl = 'http://100.105.95.54:3000';
+
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService
+  ) {}
+
+  uploadSong(formData: FormData): Observable<any> {
+    const headers = this.authService.getAuthHeaders();
+    return this.http.post(`${this.baseUrl}/songs/upload`, formData, { headers });
+  }
+}
